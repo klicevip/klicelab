b2d853aa036c9bd610babbf465195786980ed2bb
xieshengxin <xieshengxin@mallcoo.cn>
2017-05-24 14:31:43 +0800

    修改mybatis配置及初始化

--- src/main/java/klicelab/SpringConfig.java
+++ src/main/java/klicelab/SpringConfig.java
@@ -1,21 +1,12 @@
 package klicelab;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import klicelab.service.SessionService;
 import klicelab.web.filters.SessionFilter;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.TransactionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import javax.servlet.Filter;
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -25,7 +16,7 @@ import java.io.InputStream;
 @org.springframework.context.annotation.Configuration
 public class SpringConfig {
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws IOException {
+    public SqlSessionFactory sqlSessionFactory() throws IOException {
         String resource = "mybatis/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
@@ -39,17 +30,17 @@ public class SpringConfig {
 //        return sqlSessionFactory;
     }
 
-    @Bean
-    public DataSource dataSource(org.springframework.core.env.Environment environment) throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setJdbcUrl("jdbc:MySQL://localhost:3306/klicelab?serverTimezone=UTC");
-        dataSource.setUser("root");
-        dataSource.setPassword("111111");
-        dataSource.setDriverClass("com.mysql.jdbc.Driver");
-        dataSource.setMinPoolSize(4);
-        dataSource.setMaxPoolSize(50);
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource dataSource(org.springframework.core.env.Environment environment) throws PropertyVetoException {
+//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//        dataSource.setJdbcUrl("jdbc:MySQL://localhost:3306/klicelab?serverTimezone=UTC");
+//        dataSource.setUser("root");
+//        dataSource.setPassword("111111");
+//        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+//        dataSource.setMinPoolSize(4);
+//        dataSource.setMaxPoolSize(50);
+//        return dataSource;
+//    }
 
 //    @Bean
 //    public SessionFilter sessionFilter(SessionService sessionService){
